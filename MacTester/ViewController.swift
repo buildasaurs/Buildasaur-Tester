bcc2c4cf154fee1584da5a6822429b8c694b3078
Honza Dvorsky <jan.dvorsky@yahoo.com>
2015-10-14 17:40:04 +0100

    added mac and watch targets

---
+++ MacTester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacTester
+//
+//  Created by Honza Dvorsky on 10/14/15.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
